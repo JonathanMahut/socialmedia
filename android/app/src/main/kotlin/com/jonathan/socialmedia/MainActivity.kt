02f418ece362010bac1a901d74f6d7d059d06019
jonathan.mahut <jonathan.mahut@gmail.com>
2024-05-24 08:51:06 +0000

    Theme base implementation

---
+++ android/app/src/main/kotlin/com/jonathan/socialmedia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jonathan.socialmedia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,17 @@ import 'package:social_media_app/components/life_cycle_event_handler.dart';
 import 'package:social_media_app/firebase_options.dart';
 import 'package:social_media_app/landing/landing_page.dart';
 import 'package:social_media_app/screens/mainscreen.dart';
+import 'package:social_media_app/screens/mainscreenclient.dart';
+import 'package:social_media_app/screens/mainscreentatooartist.dart';
 import 'package:social_media_app/services/auth_service.dart';
 import 'package:social_media_app/services/user_service.dart';
+import 'package:social_media_app/theme_observer.dart';
 import 'package:social_media_app/utils/constants.dart';
 import 'package:social_media_app/utils/providers.dart';
 import 'package:social_media_app/view_models/theme/theme_view_model.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +26,11 @@ void main() async {
   // FirebaseAppCheck.instance // Only needed if you use App checks
   //     .installAppCheckProviderFactory(AppCheckProviderFactory(
   //         provider: SafetyNetAppCheckProviderFactory()));
+  //  BlocOverrides.runZoned(
+  //   () => runApp(const MyApp()),
+  //   blocObserver: ThemeBlocObserver(),
+  // );
+  Bloc.observer = ThemeBlocObserver();
   runApp(const MyApp());
 }
 
@@ -44,8 +55,8 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    AuthService auth = AuthService();
-    String? currentUserType;
+    // AuthService auth = AuthService();
+    // String? currentUserType ='';
     return MultiProvider(
       providers: providers,
       child: Consumer<ThemeProvider>(
@@ -72,24 +83,28 @@ class _MyAppState extends State<MyApp> {
               stream: FirebaseAuth.instance.authStateChanges(),
               builder: ((BuildContext context, snapshot) {
                 // User user = auth.getCurrentUser();
+              
+
                 if (snapshot.hasData) {
-                  //   // Test if current user userType is CLIENT or TATTOARTIST
-                  //   await FirebaseFirestore.instance
-                  //       .collection('users')
-                  //       .doc(user.uid)
-                  //       .get()
-                  //       .then(
-                  //         (value) => currentUserType = value.get('userType'),
-                  //       );
-                  //   if (currentUserType == 'CLIENT') {
-                  //     return const TabScreenClient();
-                  //   } else if (currentUserType == 'TATTOOARTIST') {
-                  //     return const TabScreenTatooArtist();
-                  //   } else if (currentUserType == 'UNKNOWN') {
-                  //     return const TabScreen();
-                  //   } else {
-                  //     return const TabScreen();
-                  //   }
+//                   // Test if current user userType is CLIENT or TATTOARTIST
+//                    FirebaseFirestore.instance
+//                     .collection('users')
+//                     .doc(user.uid)
+//                     .get()
+//                     .then(
+//                       (value) => currentUserType = value.get('userType'),
+//                     );
+// // Suggested code may be subject to a license. Learn more: ~LicenseLog:14650707.
+//                     print(currentUserType);
+//                     if (currentUserType == 'CLIENT') {
+//                       return const TabScreenClient();
+//                     } else if (currentUserType == 'TATTOOARTIST') {
+//                       return const TabScreenTatooArtist();
+//                     } else{
+//                       return const TabScreen();
+//                   }
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2308876756.
+
                   return const TabScreen();
                 } else {
                   return const Landing();
