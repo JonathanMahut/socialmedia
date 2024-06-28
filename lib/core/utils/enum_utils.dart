dd76146ad001e110252aa337ec72bf40ef3a3c23
JonathanMahut <jonathan.mahut@gmail.com>
2024-06-28 21:30:37 +0200

    Some imrovements

---
+++ lib/core/utils/enum_utils.dart
@@ -0,0 +1,26 @@
+import 'package:social_media_app/data/models/enum/gender_type.dart';
+import 'package:social_media_app/data/models/enum/user_type.dart';
+
+// Helper method to convert a string to UserType enum
+UserType? _userTypeFromString(String? userTypeString) {
+  if (userTypeString != null) {
+    try {
+      return UserType.values.byName(userTypeString);
+    } catch (e) {
+      print('Invalid UserType: $userTypeString');
+    }
+  }
+  return null;
+}
+
+// Helper method to convert a string to GenderTypeenum
+GenderType? _genderTypeFromString(String? genderString) {
+  if (genderString != null) {
+    try {
+      return GenderType.values.byName(genderString);
+    } catch (e) {
+      print('Invalid GenderType: $genderString');
+    }
+  }
+  return null;
+}
